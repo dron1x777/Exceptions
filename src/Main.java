1f3bc9e48b033e299f4865d76fdf38abba92de74
dron1x <usaamidin@gmail.com>
2026-03-10 17:45:10 +0600

    Class work

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Order order = new Order("broo", 6, 3884);
-        try {
-            order.placeOrder();
-        } catch (InvalidPriceException e) {
-            System.out.println(e.getMessage());
-        } catch (InvalidQuantityException e) {
-            System.out.println(e.getMessage());
-        }
+        Random rand = new Random();
+        Set<Student> students = new HashSet<>(Arrays.asList(
+                new Student("name", 15),
+                new Student("name", 15),
+                new Student("name", 15),
+                new Student("name", 15),
+                new Student("name", 15)
+                ));
+
+        System.out.println(students);
     }
 }
